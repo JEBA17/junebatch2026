bbd0a6c7771d0d3bc397f0b36571a23072164494
JEBA17 <prafullakl1997@gmail.com>
2026-06-03 10:14:52 +0530

    test

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,22 +1,3 @@
-module "public_ecr" {
-  source = "terraform-aws-modules/ecr/aws"
-
-  repository_name = "public-example"
-  repository_type = "public"
-
-  repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
-
-  public_repository_catalog_data = {
-    description       = "Docker container for some things"
-    about_text        = file("${path.module}/files/ABOUT.md")
-    usage_text        = file("${path.module}/files/USAGE.md")
-    operating_systems = ["Linux"]
-    architectures     = ["x86"]
-    logo_image_blob   = filebase64("${path.module}/files/clowd.png")
-  }
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+resource "aws_ecr_repository" "ecr_repo" {
+  name = var.ecr_repository_name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "s3bucket_id" {
 output "s3bucket_arn" {
   value = module.s3bucket.aws_s3_bucket_arn
 }
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.ecr_repo.repository_url
+} 
